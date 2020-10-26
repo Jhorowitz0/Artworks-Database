8b3082bc7edd01a053e562fb0c74ef791548c90a
Jhorowitz0 <46899492+Jhorowitz0@users.noreply.github.com>
2020-10-26 16:31:21 -0400

    testing database connection

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -5,8 +5,18 @@ const Artwork = mongoose.model('Artwork', models.artworkSchema);
 
 const controller = {
     renderArtworksList: (req,res)=>{
-        res.send('artworks rendered');
-    }
+        res.send('artworks rendered and updated');
+    },
+
+    addArtwork: (req,res) => {
+        let newArtwork = new Artwork(req.body);
+
+        newArtwork.save((err,artwork) => {
+            if(err) res.send(err);
+
+            res.send(artwork);
+        });
+    },
 }
 
 export default controller;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,9 @@ import controller from '../controllers/controller';
 const routes = (server) => {
     server.route('/')
         .get(controller.renderArtworksList);
+
+    server.route('/add')
+        .post(controller.addArtwork);
 }
 
 export default routes;
