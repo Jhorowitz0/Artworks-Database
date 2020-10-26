5b81b47a7257ee4543a0c71d1c44c04c1f4fd801
Jhorowitz0 <46899492+Jhorowitz0@users.noreply.github.com>
2020-10-26 07:37:29 -0400

    git webhook test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Artworks Database Online..');
+    res.send('Artworks Database Online...with UPDATES');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
